cb2983e759e41d2f99b637d83f109ea40648796f
OlivierNDev <it.deboss019@gmail.com>
2024-12-22 01:58:08 -0500

    added Patient form CSS

--- src/Patient.js
+++ src/Patient.js
@@ -1,40 +1,68 @@
-import React from 'react'
+import React from 'react';
+import './Patient.css';
 
 function Patient() {
   return (
     <div className='patient-space'>
       <form>
-        <input className='p-input' type="text" placeholder='enter name' required/>
-        <input className='p-input' type="number" placeholder='enter age' required/>
-        <input className='p-input' type="text" placeholder='enter patientID' required/>
-        <input className='p-input' type="date" placeholder='enter birthday' required/>
-        <input className='p-input' type="text" placeholder='gender' required/>
-        <input className='p-input' type="text" placeholder='phone' required/>
-        <input className='p-input' type="text" placeholder='address'/>
-        <select>
-          <option value = "disabled selected"> Select Blood Type </option>
-          <option value = "A+"> A+ </option>
-          <option value = "A-"> A- </option>
-          <option value = " B+"> B+ </option>
-          <option value = "B"> B- </option>
-          <option value="O+"> O+</option>
-          <option value="O-"> O-</option>
-          <option value="AB+"> AB+</option>
-          <option value="AB-"> AB-</option>
-        </select>
-        <input className='p-input' type="text" placeholder='weight'/>
-        <input className='p-input' type="text" placeholder='height'/>
-        <textarea className='p-input'>Enter allergies</textarea>
-        <textarea className='p-input'>Enter Medications</textarea>
-        <textarea className='p-input'>Enter diseases</textarea>{/* chronical */}
-        <input className='p-input' type="text" placeholder='emergency contact'/>
-        <input className='p-input' type="text" placeholder='insurance'/>
-        {/* <input className='p-input' type="text" placeholder='appointments'/> FOR LATER CODING */}
-        {/* <input className='p-input' type="text" placeholder='previous visits'/> FLC . . . */}
-        <button className='p-button'>Submit</button>
+        {/* First Row */}
+        <div className="field-container">
+          <input className='p-input' type="text" placeholder='Enter Name' required />
+          <input className='p-input' type="number" placeholder='Enter Age' required />
+          <input className='p-input' type="text" placeholder='Enter Patient ID' required />
+        </div>
+
+        {/* Second Row */}
+        <div className="field-container">
+          <input className='p-input' type="date" placeholder='Enter Birthday' required />
+          <input className='p-input' type="text" placeholder='Gender' required />
+          <input className='p-input' type="text" placeholder='Phone' required />
+        </div>
+
+        {/* Third Row */}
+        <div className="field-container">
+          <input className='p-input' type="text" placeholder='Address' />
+          <select className='p-input'>
+            <option value="disabled selected">Select Blood Type</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+          </select>
+        </div>
+
+        {/* Fourth Row */}
+        <div className="field-container">
+          <input className='p-input' type="text" placeholder='Weight' />
+          <input className='p-input' type="text" placeholder='Height' />
+        </div>
+
+        {/* Fifth Row */}
+        <div className="field-container">
+          <textarea className='p-input' placeholder='Enter Allergies'></textarea>
+          <textarea className='p-input' placeholder='Enter Medications'></textarea>
+        </div>
+
+        {/* Sixth Row */}
+        <div className="field-container">
+          <textarea className='p-input' placeholder='Enter Diseases'></textarea>
+          <input className='p-input' type="text" placeholder='Emergency Contact' />
+        </div>
+
+        {/* Seventh Row */}
+        <div className="field-container">
+          <input className='p-input' type="text" placeholder='Insurance' />
+        </div>
+
+        {/* Submit Button */}
+        <button className='p-button' type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Patient
+export default Patient;
